4420efe976234be9b7f32a72436962de96c67b97
mmegahed <mmegahed@student.tgm.ac.at>
2024-10-14 15:09:20 +0200

    WortTrainerGUI & Main

--- WorttrainerReloaded/src/main/java/org/example/Main.java
+++ WorttrainerReloaded/src/main/java/org/example/Main.java
@@ -1,19 +1,23 @@
 package org.example;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.ArrayList;
+import java.util.List;
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Eingabe with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
-
-        // Press Umschalt+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Umschalt+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Strg+F8.
-            System.out.println("i = " + i);
+        System.out.println("Hello world!");
+        WordspellTrainer t = new WordspellTrainer();
+        List<WordPair> words = new ArrayList<>();
+        words.add(new WordPair("https://images.eatsmarter.de/sites/default/files/styles/576x432/public/apfel-576x432.jpg", "Apfel"));
+        words.add(new WordPair("https://www.kindersache.de/sites/default/files/styles/teaser/public/banana-42793_1280.jpg?itok=TbRV17I6", "Banane"));
+        words.add(new WordPair("https://www.online-gartencenter.at/8738/pfirsich-kernechte-von-vorgebirge.jpg", "Pfirsich"));
+        words.add(new WordPair("https://cdn.gurkerl.at/images/grocery/products/7957/7957-1603471900844.jpg", "Ananas"));
+        t.addWordList(words);
+        t.pickRandom();
+        String[] options = {"Abschicken", "Beenden"};
+        boolean beendet = false;
+        WordTrainerGUI frame = new WordTrainerGUI(t);
+        while(!beendet) {
+            frame.setImage(t.getPickedWord().getImageURL());
         }
     }
 }
---
+++ WorttrainerReloaded/src/main/java/org/example/WordTrainerGUI.java
@@ -0,0 +1,70 @@
+package org.example;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.MalformedURLException;
+import java.net.URL;
+public class WordTrainerGUI extends JFrame {
+    private JPanel panel, userInteraction, buttons;
+    private JTextField tfield;
+    private JLabel image;
+    public WordTrainerGUI(WordspellTrainer trainer){
+        super("Wordtrainer");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(1000,600);
+        this.setVisible(true);
+        panel = new JPanel();
+        userInteraction = new JPanel();
+        buttons = new JPanel();
+        tfield = new JTextField();
+        image = new JLabel(new ImageIcon(""));
+        JButton submit = new JButton("Abschicken");
+        JButton exit = new JButton("Beenden");
+        panel.setLayout(new BorderLayout());
+        userInteraction.setLayout(new BoxLayout(userInteraction, BoxLayout.PAGE_AXIS));
+        buttons.setLayout(new BoxLayout(buttons, BoxLayout.LINE_AXIS));
+        this.add(panel);
+        panel.add(userInteraction, BorderLayout.SOUTH);
+        panel.add(image, BorderLayout.CENTER);
+        userInteraction.add(tfield);
+        userInteraction.add(buttons);
+        buttons.add(submit);
+        buttons.add(exit);
+        submit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+                if(trainer.check(tfield.getText()))
+                    JOptionPane.showMessageDialog(null, "Richtig!");
+                else
+                    JOptionPane.showMessageDialog(null, "Falsch!");
+                JOptionPane.showMessageDialog(null, trainer.getStatistic());
+                tfield.setText("");
+            }
+        });
+        exit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                WordTrainerManager persistence = new JSONSaveManager();
+                //persistence.save(trainer);
+                System.exit(0);
+            }
+        });
+    }
+    public void setImage(String picture){
+        try {
+            ImageIcon imageIcon = new ImageIcon(new URL(picture));
+            int orWidth = imageIcon.getIconWidth();
+            int orHeight = imageIcon.getIconHeight();
+            double widthRatio = (double) image.getWidth()/orWidth;
+            double heightRatio = (double) image.getHeight()/orHeight;
+            double scaleFactor = Math.min(widthRatio, heightRatio);
+            int scaledWidth = (int) (imageIcon.getIconWidth() * scaleFactor);
+            int scaledHeight = (int) (imageIcon.getIconHeight() * scaleFactor);
+            image.setIcon(new ImageIcon(imageIcon.getImage().getScaledInstance(scaledWidth, scaledHeight, Image.SCALE_DEFAULT)));
+        } catch (MalformedURLException e){
+            throw new RuntimeException(e);
+        }
+    }
+}
